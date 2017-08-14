8cb8ef4e2dac7dd1f5feb403087d72592cd40cf2
Adam Dziedzic <adam1dziedzic@gmail.com>
2017-08-14 23:39:19 +0200

    v1.1.1

--- js/login.js
+++ js/login.js
@@ -1,20 +1,11 @@
 (function(){
 
-	// const button = document.getElementsByTagName('button')[0];
-	// const input = document.getElementsByTagName('input')[0];
-
-	// const button = document.querySelector('.login');
-	// const input = document.querySelector('.submit');
-
-	// const button = document.querySelectorAll('.login')[0];
-	// const input = document.querySelectorAll('.submit')[0];
-
-	// const button = document.getElementByID('button')[0];
-	// const input = document.getElementByID('input')[0];
-
 	const button = document.querySelectorAll('.button_go')[0];
 	const input = document.querySelectorAll('.submit')[0];
 	const login = document.querySelectorAll('.login_input')[0];
+	const error_message = document.querySelectorAll('.error_message')[0];
+	const error_text = document.querySelectorAll('.error_text')[0];
+	const form = document.querySelectorAll('.main_form')[0];
 
 	login.addEventListener('blur', event => {
 		if (login.value.length > 1){
@@ -26,25 +17,41 @@
 
 	button.addEventListener('click', event => {
 		event.preventDefault(); // zapobiega wczytaniu strony od nowa
+		$.ajax({
+  			type: "post",
+  			data: {
+    			login: login.value,
+   				password: input.value
+ 			 },
+ 			 url: "https://efigence-camp.herokuapp.com/api/login",
+ 			 error: function(response) {
+  			  console.log(response.responseJSON.message);
+  			  error_text.innerHTML = response.responseJSON.message;
+  			  error_message.style.display = "";
+  			  $("form").css("padding-bottom", "210px");
+  			},
+  			success: function(response) {
+   			 console.log(response);
+   			 error_message.style.display = "none";
+ 			 }
+		});
+
 		if (input.value.length <1)
 		{
-			document.querySelectorAll('.submit')[0].style.border = "3px solid #c00000";
-			document.querySelectorAll('.submit')[0].value = "Wprowadź hasło";
-			document.querySelectorAll('.submit')[0].style.color = "#c00000";
+			input.style.border = "3px solid #c00000";
 		}
-		else document.querySelectorAll('.submit')[0].style.border = "none";
+		else {
+			input.style.border = "none";
+		}
 		if (login.value.length <1)
 		{
-			document.querySelectorAll('.login_input')[0].style.border = "3px solid #c00000";
-			document.querySelectorAll('.login_input')[0].value = "Wprowadź login";
-			document.querySelectorAll('.login_input')[0].style.color = "#c00000";
+			login.style.border = "3px solid #c00000";
 		}
-		else document.querySelectorAll('.login_input')[0].style.border = "none";
+		else {login.style.border = "none";
+		}
 
-		if (input.value.length <1){
-			if (login.value.length <1) location.href = 'index.html';
-		};
 	});
+		
 
 	input.addEventListener('click', event => {
 		input.value = "";
@@ -54,9 +61,5 @@
 	});
 
 
-
-	// button.addEventListener('click', function(event){
-
-	//}); 
-
 })();
+
